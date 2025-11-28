0fdb13766d9e563f1c0a821a936e1fe9bf19b6bd
dKesar <klimenkod883@gmail.com>
2025-11-28 21:18:22 +0300

    Основной функционал фронтенда

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Не найден элемент с id="root". Убедитесь, что index.html содержит <div id="root"></div>')
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
